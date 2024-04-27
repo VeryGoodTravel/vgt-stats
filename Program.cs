227394869b2a0d838c13ef21b5841d4baca1444c
Kamil Banach <banachkamil@outlook.com>
2024-04-28 01:21:37 +0200

    Project type changed to ASP.NET, logging adjusted

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using NLog;
+﻿using NLog;
 using NLog.Extensions.Logging;
 
 IConfigurationRoot? config = null;
@@ -16,9 +15,21 @@ catch (InvalidDataException e)
     Environment.Exit(0);
 }
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 try
 {
-    LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    var options = new NLogProviderOptions();
+    options.AutoShutdown = true;
+    options.Configure(config.GetSection("NLog"));
+    builder.Logging.AddNLog(options);
 }
 catch (InvalidDataException e)
 {
@@ -26,8 +37,44 @@ catch (InvalidDataException e)
     Environment.Exit(0);
 }
 
-LogManager.AutoShutdown = true;
+var app = builder.Build();
 
 var logger = LogManager.GetCurrentClassLogger();
 
 logger.Info("Hello word");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
